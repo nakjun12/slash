cf4b315c657269807f7a62bb5238a3062171ca26
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-01-18 17:06:00 +0900

    OpenGraph : test 코드 작성시 오류 catch 할 수 있도록 작성하는 법과 설명 추가

--- packages/react/react/src/components/OpenGraph/index.spec.tsx
+++ packages/react/react/src/components/OpenGraph/index.spec.tsx
@@ -28,6 +28,15 @@ describe('OpenGraph', () => {
   });
   it(`should throw error when no meta tag is found `, () => {
     render(<OpenGraph />);
+    // 오류 확인하는 법
+    //expect 문 직접 호출되면, 오류가 expect 함수에 도달하기 전에 발생하고 테스트는 실패
     expect(() => getMeta('og:title')).toThrowError();
   });
 });
+
+// () => getMeta('og:title'): 이것은 함수를 선언하고 호출하는 것입니다.
+//이 경우, 함수가 정의되고 즉시 호출됩니다. 즉, 함수 내부 코드가 선언되자마자 실행됩니다.
+
+// 그냥 getMeta('og:title'): 이것은 함수 호출입니다. 함수가 이미 정의되어 있고,
+//이 호출이 코드 어딘가에서 발생할 때 함수 내부 코드가 실행됩니다. 이 때 오류가 발생하면,
+//해당 코드 위치에서 오류가 발생하는 것이 아니라 함수 내부에서 발생하게 됩니다.
