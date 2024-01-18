ce34d2668ea9a29af6527c5f27ef4cf662237a93
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-01-18 10:42:28 +0900

    jest useFakeTimers : setTimeout 모의 함수 주석 추가

--- packages/react/react/src/components/DebounceClick/DebounceClick.spec.tsx
+++ packages/react/react/src/components/DebounceClick/DebounceClick.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import React from 'react';
 import { DebounceClick } from './DebounceClick';
 
 interface TestButtonProps {
@@ -17,12 +18,15 @@ const TestButton = ({ children, onCTAClick }: TestButtonProps) => {
 
 beforeAll(() => {
   jest.useFakeTimers();
+  //setTimeout 모의 함수
 });
 
 describe('DebounceClick', () => {
   it('should debounce "onClick" event prop passed to child element by default', async () => {
     const user = userEvent.setup({ delay: null });
+    //delay null은 이벤트 사이에 시간 지연을 두지 않겠다는 의미
     const mockFn = jest.fn();
+    //onClick 이벤트가 호출되는 횟수를 세는 모의 함수
 
     render(
       <DebounceClick wait={COMPLETION_WAIT_TIME}>
@@ -35,9 +39,11 @@ describe('DebounceClick', () => {
     await user.click(button);
 
     jest.advanceTimersByTime(BEFORE_COMPLETION_TIME);
+    //advanceTimersByTime은 모의된 시간만큼 시간 진행하는 함수
     expect(mockFn).toBeCalledTimes(0);
 
     jest.advanceTimersByTime(REMAINING_TIME);
+    //debounceClick의 wait 시간이 지나면 mockFn이 호출되는 것을 확인
     expect(mockFn).toBeCalledTimes(1);
 
     await user.click(button);
@@ -47,6 +53,7 @@ describe('DebounceClick', () => {
 
     jest.advanceTimersByTime(REMAINING_TIME);
     expect(mockFn).toBeCalledTimes(2);
+    //시간이 지나면 mockFn이 다시 호출되는 것을 확인
   });
 
   it('should debounce event prop of child element with the same name as capture', async () => {
